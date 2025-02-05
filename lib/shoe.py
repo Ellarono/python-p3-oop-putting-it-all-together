dc910d67dd284657f410bf3098e8e115681424b4
Daniella Rono <daniellarono@gmail.com>
2025-02-05 23:22:44 +0300

    all tests are passing

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None  # Use a private attribute to enable validation
+        self.size = size  # Trigger the setter
+        self.condition = None  # Initialize the condition attribute
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
